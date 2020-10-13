903e4104df2d4a6c728a7866a7383fd1a0202294
drugovvg <drugovv@gmail.com>
2020-10-13 21:47:40 +0700

    add some comments to the code

--- src/main/java/lesson03/task01/Main.java
+++ src/main/java/lesson03/task01/Main.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        /**
+         * Initialize a MathBox
+         */
 //        Number[] testArray = new Number[]{1.0, 1.0, 2.0, 3.0, 4.0};
 //        Number[] testArray = new Number[]{1, 1, 2, 3, 4};
 //        Integer[] testArray = new Integer[]{1, 1, 2, 3, 4};
         Short[] testArray = new Short[]{1, 1, 2, 3, 4};
         MathBox mathBox = new MathBox(testArray);
 
+        /**
+         * Print original content of the aSet in the MathBox and it is sum
+         */
         System.out.println("Original:");
         for (Object item :
                 mathBox.aSet) {
@@ -18,6 +24,9 @@ public class Main {
         }
         System.out.println("Sum:" + mathBox.summator());
 
+        /**
+         * Let's test removing of an item by value
+         */
         System.out.println("After removing 3:");
         mathBox.remove(3);
         for (Object item :
@@ -26,7 +35,9 @@ public class Main {
         }
         System.out.println("Sum:" + mathBox.summator());
 
-
+        /**
+         * Let's test the splitter method
+         */
         System.out.println("Splitter:");
         mathBox.splitter(2);
 
@@ -35,6 +46,9 @@ public class Main {
             System.out.println(item);
         }
 
+        /**
+         * Let's check overridden methods
+         */
         System.out.println("To String: " + mathBox.toString());
         System.out.println("Hash code: " + mathBox.hashCode());
         System.out.println("equals: " + mathBox.equals(mathBox));
--- src/main/java/lesson03/task01/MathBox.java
+++ src/main/java/lesson03/task01/MathBox.java
@@ -2,19 +2,51 @@ package lesson03.task01;
 
 import java.util.*;
 
+/**
+ * MathBox is a class with implementation of some simple math methods on aSet HashSet
+ */
 public class MathBox {
 
     HashSet aSet;
-    private Object Float;
 
+    /**
+     * Getter for the aSet
+     * @return
+     */
+    public HashSet getaSet() {
+        return aSet;
+    }
+
+    /**
+     * Setter for the aSet
+     * @return
+     */
+    public void setaSet(HashSet aSet) {
+        this.aSet = aSet;
+    }
+
+    /**
+     * MathBox constructor
+     * @param anArray - initialize aSet
+     * @param <T>
+     */
     public <T extends Number> MathBox(T[] anArray) {
-        aSet = new HashSet(Arrays.asList(anArray));
+        setaSet(new HashSet(Arrays.asList(anArray)));
     }
 
+    /**
+     * This method purpose is to get sum of the all elements in the aSet.
+     * @return sum of the all elements in the aSet
+     */
     public double summator() {
-        return aSet.stream().mapToDouble(num -> Double.valueOf(num.toString())).sum();
+        return getaSet().stream().mapToDouble(num -> Double.valueOf(num.toString())).sum();
     }
 
+    /**
+     * This method purpose is to divide all the elements in the aSet by the divider parameter and replace it with results of dividing.
+     * @param divider
+     * @param <T1>
+     */
     public <T1 extends Number> void splitter(T1 divider) {
         double doubleDivider = Double.valueOf(divider.toString());
         double[] doubles = aSet.stream().mapToDouble(num -> {
@@ -28,21 +60,27 @@ public class MathBox {
 
     }
 
-    public void remove(Integer index) {
+    /**
+     * Removing certain element in the aSet by value
+     * @param value - value to searhc and remove
+     */
+    public void remove(Integer value) {
 
         Object firstASet = aSet.stream().findFirst().get();
-//        aSet.remove(firstASet.getClass().cast(index));
 
         if(firstASet instanceof Double || firstASet instanceof Float) {
-            aSet.remove(index.doubleValue());
+            aSet.remove(value.doubleValue());
         } else if (firstASet instanceof Short) {
-            aSet.remove(index.shortValue());
+            aSet.remove(value.shortValue());
         } else {
-            aSet.remove(Integer.valueOf(index.toString()));
-//            aSet.remove((Number)index);
+            aSet.remove(Integer.valueOf(value.toString()));
         }
     }
 
+    /**
+     * toString override
+     * @return Resul of concatenation of all items in the aSet
+     */
     @Override
     public String toString() {
 
@@ -55,11 +93,19 @@ public class MathBox {
 
     }
 
+    /**
+     * Hash Code override based on toString() method
+     * @return hash code
+     */
     @Override
     public int hashCode() {
         return this.toString().hashCode();
     }
 
+    /**
+     * Equals method override based on toString() method
+     * @return boolean if an obj is equal to the MathBox
+     */
     @Override
     public boolean equals(Object obj) {
         return this.toString().equals(obj.toString());
